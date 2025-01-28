717b7df8bd88729bbcf6e55b44011c9d268e3eb0
Kennedy Nganga <ken.w.nganga@gmail.com>
2025-01-28 12:57:16 +0300

    Update layout.tsx
    added tailwind stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '../styles/globals.css'; // Import Tailwind CSS global styles
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-900 text-white">
+        {children}
+      </body>
     </html>
   );
 }
