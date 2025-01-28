5913aef8fd997cb17be1cea6a942d5dccc2f6878
Kennedy Nganga <ken.w.nganga@gmail.com>
2025-01-28 06:27:34 +0300

    Update map.tsx
    replaced geoserver url with basemap

--- components/map.tsx
+++ components/map.tsx
@@ -1,22 +1,18 @@
 'use client';
-import { MapContainer, TileLayer, WMSTileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
   return (
     <MapContainer
-      center={[-1.2921, 36.8219]}
+      center={[-1.2921, 36.8219]} // Centered on Nairobi, Kenya
       zoom={8}
       className="h-[70vh] lg:h-[80vh] w-full rounded-xl"
     >
+      {/* Placeholder base map layer */}
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-      />
-      <WMSTileLayer
-        url={process.env.NEXT_PUBLIC_GEOSERVER_URL}
-        layers="shamba:base_satellite"
-        format="image/png"
-        transparent={true}
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
     </MapContainer>
   );
