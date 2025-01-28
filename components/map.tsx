27d2558a12d123471fb3d4adcffc968ada5d198f
Kennedy Nganga <ken.w.nganga@gmail.com>
2025-01-28 18:09:51 +0300

    Update map.tsx
    improved map

--- components/map.tsx
+++ components/map.tsx
@@ -1,29 +1,54 @@
 'use client';
+import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
 
-const Map = React.memo(() => {
+const { BaseLayer } = LayersControl;
+
+export default function Map() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <div>Loading map...</div>;
+  }
+
   return (
     <div
       style={{
         height: '100%',
         width: '100%',
         backgroundColor: '#111',
+        position: 'relative',
       }}
     >
       <MapContainer
-        center={[-1.2921, 36.8219]} // Nairobi, Kenya
-        zoom={8}
+        center={[-1.065, 37.135]} // Coordinates for Kilimambogo area
+        zoom={13} // Zoom level
         style={{ height: '100%', width: '100%' }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+        <LayersControl position="topright">
+          {/* OpenStreetMap Basemap */}
+          <BaseLayer checked name="OpenStreetMap">
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </BaseLayer>
+
+          {/* Google Earth Imagery Basemap */}
+          <BaseLayer name="Google Earth Imagery">
+            <TileLayer
+              url="http://www.google.cn/maps/vt?lyrs=s&x={x}&y={y}&z={z}"
+              attribution='&copy; <a href="https://www.google.com/maps">Google Maps</a>'
+            />
+          </BaseLayer>
+        </LayersControl>
       </MapContainer>
     </div>
   );
-});
-
-export default Map;
+}
